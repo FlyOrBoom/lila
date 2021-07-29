6def04a6871418a3697419bbab7e1c26237757a8
Xing Liu <31298876+FlyOrBoom@users.noreply.github.com>
2021-07-28 23:02:47 -0700

    add wakeLock behavior

--- ui/site/src/account.ts
+++ ui/site/src/account.ts
@@ -5,6 +5,7 @@ lichess.load.then(() => {
     ['behavior', 'arrowSnap', 'arrow.snap', 1],
     ['behavior', 'courtesy', 'courtesy', 0],
     ['behavior', 'scrollMoves', 'scrollMoves', 1],
+    ['behavior', 'wakeLock', 'wakeLock', 0],
   ];
 
   $('.security table form').on('submit', function (this: HTMLFormElement) {
